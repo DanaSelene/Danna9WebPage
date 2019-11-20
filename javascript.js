15fb7010df89b5451854a4acb70dd4bb9d3a6247
DanaSelene <dawanamo@gmail.com>
2019-11-21 00:21:12 +1100

    视频-小功能
    用internal js code添加了几个不起眼的小功能。像是暂停，开始，放大，放小。但是都是利用button来做的。放大放小也不是很如意。。
    
    先告一段落吧（因为找不到进度条，以及快进，后退的功能。第二个原因是external js code明明的internal的code一摸一样但是就是不能用。。）

--- javascript.js
+++ javascript.js
@@ -1,5 +1,6 @@
+// 下面的JS是写给button: back to top的
 //Get the button
-var mybuttin = document.getElementById("myBtn");
+var mybutton = document.getElementById("myBtn");
 
 //When the user scrolls down 20px from the top of the document, show the button
 window.onscroll=function(){
@@ -19,3 +20,31 @@ function topFunction() {
     document.body.scrollTop = 0;
     document.documentElement.scrollTop = 0;
   }
+
+
+
+
+
+// //   下面开始的JS是写给首页video tag的（在有趣功能区里）--双重comment，因为这段code只会在internal的时候工作，即 only works when it is
+    //  an internal js code
+
+// var myVideo = document.getElementById("video1"); 
+
+// function playPause() { 
+//   if (myVideo.paused) 
+//     myVideo.play(); 
+//   else 
+//     myVideo.pause(); 
+// } 
+
+// function makeBig() { 
+//     myVideo.width = 920; 
+// } 
+
+// function makeSmall() { 
+//     myVideo.width = 420; 
+// } 
+
+// function makeNormal() { 
+//     myVideo.width = 720; 
+// } 
